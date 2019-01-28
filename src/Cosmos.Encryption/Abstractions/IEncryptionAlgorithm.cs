da3d1cd1628cd43e6b3b65ac2e507d278d497580
alexinea <forerunner@outlook.com>
2019-01-28 11:22:07 +0800

    Move Cosmos.Encryption from Cosmos core repository.

---
+++ src/Cosmos.Encryption/Abstractions/IEncryptionAlgorithm.cs
@@ -0,0 +1,6 @@
+﻿namespace Cosmos.Encryption.Abstractions {
+    public interface IEncryptionAlgorithm {
+        string Encrypt(string plainText);
+        string Decrypt(string cipher);
+    }
+}
---
+++ src/Cosmos.Encryption/Asymmetric/RSAKeyTypes.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Cosmos.Encryption
+{
+    // ReSharper disable once InconsistentNaming
+    public enum RSAKeyTypes
+    {
+        XML,
+        JSON,
+        Pkcs1,
+        Pkcs8,
+    }
+}
