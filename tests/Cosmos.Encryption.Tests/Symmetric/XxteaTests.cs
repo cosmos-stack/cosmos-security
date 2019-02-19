21628d37c314c109bf04c6b1a451532280f55dd3
alexinea <forerunner@outlook.com>
2019-02-19 10:58:53 +0800

    Add XXTEA encryption provider

---
+++ tests/Cosmos.Encryption.Tests/Symmetric/XxteaTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.Encryption.Symmetric;
+using Xunit;
+
+namespace Cosmos.Encryption.Tests.Symmetric
+{
+    public class XxteaTests
+    {
+        [Fact]
+        public void Encrypt()
+        {
+            var s = XXTEAEncryptionProvider.Encrypt("AlexLEWIS", "alexinea");
+            Assert.Equal("ToAB58U3JHH24EIEWhqSIA==", s);
+        }
+
+        [Fact]
+        public void Decrypt()
+        {
+            var o = XXTEAEncryptionProvider.Decrypt("ToAB58U3JHH24EIEWhqSIA==", "alexinea");
+            Assert.Equal("AlexLEWIS", o);
+        }
+    }
+}
