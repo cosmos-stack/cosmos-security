f4993a64a3e428709b5104c6cfc4218e26530527
alexinea <forerunner@outlook.com>
2019-08-13 14:47:45 +0800

    Add extension method for SM3 hashing

---
+++ src/Cosmos.Encryption/Cosmos/Extensions/Extensions.SM3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.Encryption;
+
+namespace Cosmos.Extensions
+{
+    /// <summary>
+    /// Extensions for encryption
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// To SM3
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        // ReSharper disable once InconsistentNaming
+        public static string ToSM3(this string data, Encoding encoding = null) => SM3HashingProvider.Signature(data, encoding);
+    }
+}
