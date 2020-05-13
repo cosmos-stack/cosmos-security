3d954b6484582d0bebd0b8a2a9bdb7fc843b279b
alexinea <forerunner@outlook.com>
2020-05-13 22:12:10 +0800

    增加过密算法 SM2 Key 的创建

---
+++ src/Cosmos.Encryption/Cosmos/Encryption/Asymmetric/SM2Key.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using Cosmos.Optionals;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace Cosmos.Encryption.Asymmetric {
+    public class SM2Key {
+        internal SM2Key(ECPoint publicKey, BigInteger prvateKey, Encoding encoding = default) {
+            PublicKey = Hex.Encode(publicKey.GetEncoded()).GetString(encoding.SafeValue()).ToUpper();
+            PrivateKey = Hex.Encode(prvateKey.ToByteArray()).GetString(encoding.SafeValue()).ToUpper();
+        }
+
+        /// <summary>
+        /// SM2 public key
+        /// </summary>
+        public string PublicKey { get; set; }
+
+        /// <summary>
+        /// SM2 private key
+        /// </summary>
+        public string PrivateKey { get; set; }
+    }
+}
