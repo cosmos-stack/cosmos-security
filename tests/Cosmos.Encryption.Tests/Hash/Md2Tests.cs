099557a0272480a6979ddbe8cbdc4c0739af0756
alexinea <forerunner@outlook.com>
2020-07-17 14:59:41 +0800

    Add MD2 Hashing provider.

---
+++ tests/Cosmos.Encryption.Tests/Hash/Md2Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Cosmos.Encryption.Tests.Hash
+{
+    public class Md2Tests
+    {
+        [Fact]
+        public void Md2()
+        {
+            var signature = MD2HashingProvider.Signature("The quick brown fox jumps over the lazy dog");
+            Assert.Equal("03D85A0D629D2C442E987525319FC471", signature);
+        }
+    }
+}
