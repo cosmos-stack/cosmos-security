069f241b2bd1f679b876b07bfdd66816951f815c
alexinea <forerunner@outlook.com>
2021-04-13 15:57:23 +0800

    Reafctor DJBX33A/Time33 Hash Function and add UT.

---
+++ src/Cosmos.Security.Verification/Cosmos/Security/Verification/DJB/Time33Factory.cs
@@ -0,0 +1,9 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Cosmos.Security.Verification
+{
+    public static class Time33Factory
+    {
+        public static Time33Function Create() => new();
+    }
+}
---
+++ tests/CosmosVerificationUT/DjbUT/Time33Tests.cs
@@ -0,0 +1,21 @@
+﻿using Cosmos.Security.Verification;
+using Shouldly;
+using Xunit;
+
+namespace DjbUT
+{
+    [Trait("DjbUT", "Time33Tests")]
+    public class Time33Tests
+    {
+        [Theory(DisplayName = "DJBX33A/Time33")]
+        [InlineData("image", "A87CA80F")]
+        [InlineData("image0", "D811B804")]
+        [InlineData("image1", "D911B804")]
+        public void Time33Test(string data, string hex)
+        {
+            var function = Time33Factory.Create();
+            var hashVal = function.ComputeHash(data);
+            hashVal.AsHexString(true).ShouldBe(hex);
+        }
+    }
+}
