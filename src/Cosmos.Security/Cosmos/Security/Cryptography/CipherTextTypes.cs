c6d5e38b58e67ca608f5df251a32892cde9a2981
alexinea <forerunner@outlook.com>
2021-05-06 15:28:30 +0800

    Update CipherTextTypes enum and a set of extension methods for it.

--- src/Cosmos.Security/Cosmos/Security/Cryptography/CipherTextTypes.cs
+++ src/Cosmos.Security/Cosmos/Security/Cryptography/CipherTextTypes.cs
@@ -1,4 +1,10 @@
-﻿namespace Cosmos.Security.Cryptography
+﻿using System;
+using System.Text;
+using Cosmos.Conversions;
+using Cosmos.Optionals;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace Cosmos.Security.Cryptography
 {
     public enum CipherTextTypes
     {
@@ -8,6 +14,27 @@
         Base91Text,
         Base256Text,
         ZBase32Text,
+        Hex,
         Custom
+    }
+
+    public static class CipherTextTypeExtensions
+    {
+        public static byte[] GetBytes(this CipherTextTypes signatureTextType, string signature, Encoding encoding = default, Func<string, byte[]> customCipherTextConverter = null)
+        {
+            encoding = encoding.SafeEncodingValue();
+
+            return signatureTextType switch
+            {
+                CipherTextTypes.PlainText => encoding.GetBytes(signature),
+                CipherTextTypes.Base32Text => BaseConv.FromBase32(signature),
+                CipherTextTypes.Base64Text => BaseConv.FromBase64(signature),
+                CipherTextTypes.Base91Text => BaseConv.FromBase91(signature),
+                CipherTextTypes.Base256Text => BaseConv.FromBase256(signature),
+                CipherTextTypes.ZBase32Text => BaseConv.FromZBase32(signature),
+                CipherTextTypes.Hex => Hex.Decode(signature),
+                _ => customCipherTextConverter is null ? encoding.GetBytes(signature) : customCipherTextConverter(signature)
+            };
+        }
     }
 }
